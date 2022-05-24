01d36c897275b74442cede5219a9a9797f9586b1
Vipin Kumar <vipinthakur3881@gmail.com>
2022-05-25 00:53:50 +0530

    added NewExpense Component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, {useState} from 'react';
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
@@ -6,14 +8,27 @@ function ExpenseItem(props) {
   // const expenseDate = new Date(2022, 5, 16);
   // const expenseTitle = "Car Insurance";
   // const expenseAmount = 294.67;
+  
+  // function clickHandler(){
+  //   console.log("clicked by me");
+  // };
 
+
+
+  const [title, setTitle]=useState(props.title);
+  //console.log("Expenses item evaluated ");
+  const clickHandler=()=>{
+   setTitle('Updated!!');
+  // console.log('title');
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
